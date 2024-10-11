1e01c89f7918d56b638c8860da6888f5bda9f45c
Lauliek <laureline.pellegrini@gmail.com>
2024-10-11 10:13:35 +0200

    2e EXEMPLE

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-let monCanard = "donald";
+/* 1er EXEMPLE*/
+/* let monCanard = "donald";
 const nombreDePate = 2;
 const nombreDAile = 2;
+ */
 
-
-/* ici les paramètres ne servent à rien car on ne s'en sert pas dans la fonction*/
-function canard(nombreDePate,nombreDAile) {
+/* ici les paramètres ne servent à rien car on ne s'en sert pas dans la fonction, et le += concatène les string*/
+/* function canard(nombreDePate,nombreDAile) {
     let maCanette = "daisy";
     let mesCanards = maCanette += monCanard;
     return mesCanards;
 };
 
-console.log(canard());
+console.log(canard()); 
+affichera dans la console de l'inspection : daisydonald
+*/
+
+/* 2er EXEMPLE*/
+
+let monCanard = "donald";
+
+function canard(nombreDePate,nombreDAile) {
+    let maCanette = "daisy";
+    let mesCanards = maCanette += monCanard;
+    return nombreDePate += nombreDAile;
+};
+
+console.log(canard(2,1));
+/* affichera dans la console de l'inspection : 3 et le += ne concatène pas les nombre mais les additionne,
+pour qu'il les concatène il aurait fallu mettre les nombres en string avec "" soit : console.log(canard("2","1"));
+ */
+
+
 
