fc45a82a80dda43b3d1ff4b2a037dace7f2fec93
Lauliek <laureline.pellegrini@gmail.com>
2024-10-11 11:19:23 +0200

    3e EXEMPLE

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ console.log(canard());
 affichera dans la console de l'inspection : daisydonald
 */
 
-/* 2er EXEMPLE*/
+/* 2e EXEMPLE*/
 
-let monCanard = "donald";
+/* let monCanard = "donald";
 
 function canard(nombreDePate,nombreDAile) {
     let maCanette = "daisy";
@@ -26,9 +26,30 @@ function canard(nombreDePate,nombreDAile) {
 };
 
 console.log(canard(2,1));
-/* affichera dans la console de l'inspection : 3 et le += ne concatène pas les nombre mais les additionne,
-pour qu'il les concatène il aurait fallu mettre les nombres en string avec "" soit : console.log(canard("2","1"));
  */
+/* affichera dans la console de l'inspection : 3 et le += ne concatène pas les nombre mais les additionne,
+pour qu'il les concatène et ainsi avoir 21 il aurait fallu mettre les nombres en string avec "" soit : console.log(canard("2","1"));
+ */
+
+
+/* 3e EXEMPLE*/
+
+let monCanard = "donald";
+let x = 1;
+function f() {
+    let x = 2;
+    console.log(x);
+}
+
+console.log(x);
+f();
+
+/* affichera dans la console de l'inspection : 1 pour le console.log(x) et 2 pour le f() car le 
+second let de let x = 2 et déclaré juste dans la fonction et n'a donc pas de portée globale,
+donc le console.log affichera la première déclaration de x soit 1 ;
+pour ce qui est de f(), on appelle la fonction donc avec let x = 2 et le console.log dans la fonction;
+le fait de déclarer let x = 2 dans la fonction alors qu'il est déjà déclaré est possible car 
+le fait de déclarer à l'intérieur de la fonction crée un autre espace mémoire différent du 1er */
 
 
 
