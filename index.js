8ba3f0f908ed5b542d1e2f32b668a2b0a1509b9b
Lauliek <laureline.pellegrini@gmail.com>
2024-10-11 09:59:25 +0200

    correcteion erreur qui s'est glissée dans html

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const nombreDePate = 2;
 const nombreDAile = 2;
 
 
-/* ici les paramètres ne servent à rien car on ne s'en sert pas dans la fonction
- */function canard(nombreDePate,nombreDAile) {
+/* ici les paramètres ne servent à rien car on ne s'en sert pas dans la fonction*/
+function canard(nombreDePate,nombreDAile) {
     let maCanette = "daisy";
     let mesCanards = maCanette += monCanard;
     return mesCanards;
