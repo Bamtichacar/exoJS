0f80ee413feada33b1825ea3014006e810af28e0
Lauliek <laureline.pellegrini@gmail.com>
2024-10-11 09:51:43 +0200

    1er exemple

---
+++ index.js
@@ -0,0 +1,14 @@
+let monCanard = "donald";
+const nombreDePate = 2;
+const nombreDAile = 2;
+
+
+/* ici les paramètres ne servent à rien car on ne s'en sert pas dans la fonction
+ */function canard(nombreDePate,nombreDAile) {
+    let maCanette = "daisy";
+    let mesCanards = maCanette += monCanard;
+    return mesCanards;
+};
+
+console.log(canard());
+
